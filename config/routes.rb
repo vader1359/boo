be968b1ba156135a70b427817eefa0dc9dc1d794
vader1359 <vader1359@gmail.com>
2017-09-05 19:59:18 +0700

    Finish design my-card

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :images
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "users#test"
 
   resources :users do
-    get 'hello', on: :collection
+
   end
 
-  
+
+
+  get "/test" => "home#test"
+  root "home#index"
 end
