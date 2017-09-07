cd9c85d1916a03da5ddf72bac6804adc66654c1c
vader1359 <vader1359@gmail.com>
2017-09-07 11:01:05 +0700

    Kind of finish home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
   end
   
   def test
+    url = "https://www.behance.net/v2/projects/?api_key=dNPpNVimepzJIOPUQECK0838tQgYR2sT"
+    json_data = HTTP.get(url)
+    data = JSON.parse(json_data)
+    @design_list = data["projects"][0..99].map {|design| design["covers"]["original"]}
+    @design_list.shuffle!
   end
 end
