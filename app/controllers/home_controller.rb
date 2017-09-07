d8fbef78aa7a2ef9af65ed67682272689412908b
vader1359 <vader1359@gmail.com>
2017-09-07 13:23:34 +0700

    Fix some css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    url = "https://www.behance.net/v2/projects/?api_key=dNPpNVimepzJIOPUQECK0838tQgYR2sT"
+    json_data = HTTP.get(url)
+    data = JSON.parse(json_data)
+    @design_list = data["projects"][0..99].map {|design| design["covers"]["original"]}
+    @design_list.shuffle!
   end
   
   def test
